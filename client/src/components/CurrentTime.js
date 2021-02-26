f649acf7fbdf96b38b0f4f3f6d45592f16f1519f
becky <beckymccarthy777@gmail.com>
2021-02-26 11:34:43 -0800

    Added time incrementor

--- client/src/components/CurrentTime.js
+++ client/src/components/CurrentTime.js
@@ -2,10 +2,12 @@ const moment = require ("moment");
 
 
 const CurrentTime = () => {
-    
-
-    let curr_time = moment().format('LT');
+    let curr_time = moment().format('MMMM Do YYYY, h:mm:ss a');
     return curr_time;
 }; 
+CurrentTime();
+setInterval(() => {
+    CurrentTime();
+}, 1000);
 
 export default CurrentTime;
