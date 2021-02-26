eedddf5a260c59256e8ed6c13f44b384547e731e
becky <beckymccarthy777@gmail.com>
2021-02-26 11:44:17 -0800

    Created isPalidrome function

---
+++ client/src/helpers/isPalidrome.js
@@ -0,0 +1,7 @@
+const isPalidrome = (str) => {
+    let newStr = str.replace(/[^a-zA-Z\d]/g, '');
+    let compare = newStr.split('').reverse().join('');
+    return newStr.toLowerCase() === compare.toLowerCase();
+}
+
+export default isPalidrome;
